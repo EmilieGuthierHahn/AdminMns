cb8e429b32828ca93964ad4cc501f13287576474
Emilie Guthier <guthier.emilie@yahoo.fr>
2025-05-28 14:00:17 +0200

    28.05.25 pushing up

--- AdminMns/Program.cs
+++ AdminMns/Program.cs
@@ -33,6 +33,7 @@ builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
+
 builder.Services.AddAuthentication(options =>
     {
         options.DefaultScheme = IdentityConstants.ApplicationScheme;
